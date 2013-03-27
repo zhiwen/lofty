cc14a538844c20d7ccfd7c110086c431c65fd945
edgarhoo <edgarhoo@gmail.com>
2013-03-27 20:39:53 +0800

    rewrite dir

--- src/kernel/boot.js
+++ src/kernel/boot.js
@@ -1,4 +1,4 @@
-/** @preserve Lofty v%VERSION% %EDITION% http://lofty.fangdeng.org/ MIT */
+/** @preserve Lofty v@VERSION @EDITION http://lofty.fangdeng.org/ MIT */
 /**
  * @module lofty/kernel/boot
  * @author Edgar Hoo <edgarhoo@gmail.com>
--- tests/specs/kernel/alicn/alicn.js
+++ tests/specs/kernel/alicn/alicn.js
@@ -7,9 +7,12 @@
 describe( 'lofty/kernel/alicn', function(){
     
     var configCache = lofty.cache.config,
-        id2url = lofty.cache.kernel.id2url.exports;
+        id2url = lofty.cache.kernel.id2url;
     
-    describe( 'config配置', function(){
+    id2url && describe( 'config配置', function(){
+        
+        id2url = id2url.exports;
+        
         it( '时间戳hasStamp为true', function(){
             var a = { id: 'jioje/wwfdfe' };
             id2url(a);
@@ -22,11 +25,11 @@ describe( 'lofty/kernel/alicn', function(){
             expect(configCache.debug).toEqual(location.href.indexOf('lofty.debug=true')>0);
         } );
         
-        it( 'resolve处理lofty/makeup/sys', function(){
+        it( 'resolve处理lofty/avid/sys', function(){
             var a = { id: 'lofty/tests/a' };
             var b = { id: 'lofty/tests/a.css' };
-            var c = { id: 'makeup/tests/a' };
-            var d = { id: 'makeup/tests/a.css' };
+            var c = { id: 'avid/tests/a' };
+            var d = { id: 'avid/tests/a.css' };
             var e = { id: 'sys/tests/a' };
             var f = { id: 'sys/tests/a.css' };
             
@@ -39,8 +42,8 @@ describe( 'lofty/kernel/alicn', function(){
             
             expect(a.url.indexOf('fdevlib/js/lofty/tests/a.js')>0).toEqual(true);
             expect(b.url.indexOf('fdevlib/css/lofty/tests/a.css')>0).toEqual(true);
-            expect(c.url.indexOf('fdevlib/js/makeup/tests/a.js')>0).toEqual(true);
-            expect(d.url.indexOf('fdevlib/css/makeup/tests/a.css')>0).toEqual(true);
+            expect(c.url.indexOf('fdevlib/js/avid/tests/a.js')>0).toEqual(true);
+            expect(d.url.indexOf('fdevlib/css/avid/tests/a.css')>0).toEqual(true);
             expect(e.url.indexOf('sys/js/tests/a.js')>0).toEqual(true);
             expect(f.url.indexOf('sys/css/tests/a.css')>0).toEqual(true);
         } );
